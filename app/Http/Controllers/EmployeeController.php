c57e17cbe4ee0aa79d13f2103dc7d11e93e63218
enoyasu <yasu19991207@gmail.com>
2022-11-06 16:48:16 +0900

    OASA 一覧表示、指定id表示api作成

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use  App\Models\Employee;
+use  App\Models\EmployeeProfile;
+
+class EmployeeController extends Controller
+{
+    public function index() 
+    {
+        $emp = Employee::all();
+        // dd($employees);
+        // return view('welcome')->with('employees',$emp);
+        return response()->json($emp,200,['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8mb4'],JSON_UNESCAPED_UNICODE);
+    }
+
+    public function create(Request $request)
+    {
+        $emp = new Employee();
+        $emp_prof = new EmployeeProfile();
+
+        $emp = [
+            'name' => $request->input('name'),
+            'name_kana' => $request->input('name_kana'),
+            'gender' => $request->input('gender'),
+            'age' => $request->input('age'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+
+        $emp_prof = [
+            'emp_id' => mysql_insert_id(),
+            'h_pay' => $request->input('h_pay'),
+            'tel' => $request->input('tel'),
+            'address' => $request->input('address'),
+            'birthday' => $request->input('birthday'),
+            'memo' => $request->input('memo'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+        
+        $emp->save();
+        $emp_prof->save();
+
+        return response()->json(Employee::all());
+    }
+
+    public function show(Int $id)
+    {
+        $emp = Employee::find($id);
+        return response()->json($emp,200,['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8mb4'],JSON_UNESCAPED_UNICODE);
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [EmployeeController::class, 'index']);
+Route::post('api/employee/create', [EmployeeController::class, 'create']);
+Route::get('api/employee/{id}', [EmployeeController::class, 'show']);
